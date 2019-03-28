4ee39d11d8f37766a7e89a031b76914d66be2eb2
James Buchanan <jamesb@spectrumsystems.com>
2019-03-28 13:57:15 -0500

    added formulas page

---
+++ CEMSStudyApp/Copied Pages/Formulas.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace CEMSStudyApp
+{
+    partial class Formulas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Formulas));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxAnswer = new System.Windows.Forms.TextBox();
+            this.buttonBack = new System.Windows.Forms.Button();
+            this.buttonNext = new System.Windows.Forms.Button();
+            this.buttonToggle = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.comboBoxFormula = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(41, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Formulas";
+            // 
+            // textBoxAnswer
+            // 
+            this.textBoxAnswer.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.textBoxAnswer.Enabled = false;
+            this.textBoxAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxAnswer.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBoxAnswer.Location = new System.Drawing.Point(17, 51);
+            this.textBoxAnswer.Multiline = true;
+            this.textBoxAnswer.Name = "textBoxAnswer";
+            this.textBoxAnswer.ReadOnly = true;
+            this.textBoxAnswer.Size = new System.Drawing.Size(771, 325);
+            this.textBoxAnswer.TabIndex = 6;
+            this.textBoxAnswer.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // buttonBack
+            // 
+            this.buttonBack.BackColor = System.Drawing.Color.Black;
+            this.buttonBack.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.buttonBack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonBack.ForeColor = System.Drawing.Color.White;
+            this.buttonBack.Location = new System.Drawing.Point(17, 382);
+            this.buttonBack.Name = "buttonBack";
+            this.buttonBack.Size = new System.Drawing.Size(75, 23);
+            this.buttonBack.TabIndex = 7;
+            this.buttonBack.Tag = "Previous Question";
+            this.buttonBack.Text = "<<";
+            this.buttonBack.UseVisualStyleBackColor = false;
+            // 
+            // buttonNext
+            // 
+            this.buttonNext.BackColor = System.Drawing.Color.Black;
+            this.buttonNext.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.buttonNext.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonNext.ForeColor = System.Drawing.Color.White;
+            this.buttonNext.Location = new System.Drawing.Point(98, 382);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(75, 23);
+            this.buttonNext.TabIndex = 8;
+            this.buttonNext.Tag = "Next Question";
+            this.buttonNext.Text = ">>";
+            this.buttonNext.UseVisualStyleBackColor = false;
+            // 
+            // buttonToggle
+            // 
+            this.buttonToggle.BackColor = System.Drawing.Color.Blue;
+            this.buttonToggle.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.buttonToggle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonToggle.ForeColor = System.Drawing.Color.White;
+            this.buttonToggle.Location = new System.Drawing.Point(713, 22);
+            this.buttonToggle.Name = "buttonToggle";
+            this.buttonToggle.Size = new System.Drawing.Size(75, 23);
+            this.buttonToggle.TabIndex = 9;
+            this.buttonToggle.Tag = "Click to toggle Answer";
+            this.buttonToggle.Text = "Toggle";
+            this.buttonToggle.UseVisualStyleBackColor = false;
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.BackColor = System.Drawing.Color.Red;
+            this.buttonExit.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.buttonExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonExit.ForeColor = System.Drawing.Color.White;
+            this.buttonExit.Location = new System.Drawing.Point(713, 382);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(75, 23);
+            this.buttonExit.TabIndex = 10;
+            this.buttonExit.Tag = "Exit Application";
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = false;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // comboBoxFormula
+            // 
+            this.comboBoxFormula.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.comboBoxFormula.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxFormula.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBoxFormula.FormattingEnabled = true;
+            this.comboBoxFormula.Items.AddRange(new object[] {
+            "-- Formula Name --"});
+            this.comboBoxFormula.Location = new System.Drawing.Point(195, 24);
+            this.comboBoxFormula.Name = "comboBoxFormula";
+            this.comboBoxFormula.Size = new System.Drawing.Size(498, 21);
+            this.comboBoxFormula.TabIndex = 12;
+            // 
+            // Formulas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.SeaGreen;
+            this.ClientSize = new System.Drawing.Size(800, 419);
+            this.Controls.Add(this.comboBoxFormula);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonToggle);
+            this.Controls.Add(this.buttonNext);
+            this.Controls.Add(this.buttonBack);
+            this.Controls.Add(this.textBoxAnswer);
+            this.Controls.Add(this.label1);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Formulas";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CEMS Study App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxAnswer;
+        private System.Windows.Forms.Button buttonBack;
+        private System.Windows.Forms.Button buttonNext;
+        private System.Windows.Forms.Button buttonToggle;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.ComboBox comboBoxFormula;
+    }
+}

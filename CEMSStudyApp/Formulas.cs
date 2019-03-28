3de1dcd5b354af87d3d40bcd1b2d582f30175be3
James Buchanan <jamesb@spectrumsystems.com>
2019-03-28 13:59:16 -0500

    fixed fomula code error

---
+++ CEMSStudyApp/Formulas.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CEMSStudyApp
+{
+    public partial class Formulas : Form
+    {
+        public Formulas()
+        {
+            InitializeComponent();
+            comboBoxFormula.SelectedIndex = 0;
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("Are You Sure?", "Exit Application", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (dr == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+       
+    }
+}

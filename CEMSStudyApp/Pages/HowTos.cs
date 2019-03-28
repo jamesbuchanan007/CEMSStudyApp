ddd8dbf7cb2cace9ccb0b8745409f817052cc41d
James Buchanan <jamesb@spectrumsystems.com>
2019-03-28 14:28:33 -0500

    added how to's page

---
+++ CEMSStudyApp/Pages/HowTos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CEMSStudyApp.Pages
+{
+    public partial class HowTos : Form
+    {
+        public HowTos()
+        {
+            InitializeComponent();
+            comboBoxHowTo.SelectedIndex = 0;
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("Are You Sure?", "Exit Application", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (dr == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+       
+    }
+}

d7f0c5ec2401e765fdcea12ac70ca370b1ec4aa2
James Buchanan <jamesb@spectrumsystems.com>
2019-04-09 10:09:35 -0500

    added load tables connection method;

--- CEMSStudyApp/Pages/Acronyms.Designer.cs
+++ CEMSStudyApp/Pages/Acronyms.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Acronyms));
             this.label1 = new System.Windows.Forms.Label();
             this.textBoxAnswer = new System.Windows.Forms.TextBox();
@@ -52,10 +51,9 @@
             this.label1.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(124, 53);
-            this.label1.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label1.Location = new System.Drawing.Point(46, 22);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(267, 63);
+            this.label1.Size = new System.Drawing.Size(110, 26);
             this.label1.TabIndex = 0;
             this.label1.Text = "Acronyms";
             // 
@@ -65,12 +63,11 @@
             this.textBoxAnswer.Enabled = false;
             this.textBoxAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBoxAnswer.ForeColor = System.Drawing.SystemColors.InactiveCaption;
-            this.textBoxAnswer.Location = new System.Drawing.Point(45, 263);
-            this.textBoxAnswer.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.textBoxAnswer.Location = new System.Drawing.Point(17, 110);
             this.textBoxAnswer.Multiline = true;
             this.textBoxAnswer.Name = "textBoxAnswer";
             this.textBoxAnswer.ReadOnly = true;
-            this.textBoxAnswer.Size = new System.Drawing.Size(1334, 628);
+            this.textBoxAnswer.Size = new System.Drawing.Size(503, 266);
             this.textBoxAnswer.TabIndex = 6;
             this.textBoxAnswer.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
@@ -80,10 +77,9 @@
             this.buttonBack.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.buttonBack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonBack.ForeColor = System.Drawing.Color.White;
-            this.buttonBack.Location = new System.Drawing.Point(45, 194);
-            this.buttonBack.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.buttonBack.Location = new System.Drawing.Point(17, 81);
             this.buttonBack.Name = "buttonBack";
-            this.buttonBack.Size = new System.Drawing.Size(200, 55);
+            this.buttonBack.Size = new System.Drawing.Size(75, 23);
             this.buttonBack.TabIndex = 7;
             this.buttonBack.Tag = "Previous Question";
             this.buttonBack.Text = "<<";
@@ -96,10 +92,9 @@
             this.buttonNext.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.buttonNext.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonNext.ForeColor = System.Drawing.Color.White;
-            this.buttonNext.Location = new System.Drawing.Point(261, 194);
-            this.buttonNext.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.buttonNext.Location = new System.Drawing.Point(98, 81);
             this.buttonNext.Name = "buttonNext";
-            this.buttonNext.Size = new System.Drawing.Size(200, 55);
+            this.buttonNext.Size = new System.Drawing.Size(75, 23);
             this.buttonNext.TabIndex = 8;
             this.buttonNext.Tag = "Next Question";
             this.buttonNext.Text = ">>";
@@ -112,10 +107,9 @@
             this.buttonToggle.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.buttonToggle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonToggle.ForeColor = System.Drawing.Color.White;
-            this.buttonToggle.Location = new System.Drawing.Point(1179, 194);
-            this.buttonToggle.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.buttonToggle.Location = new System.Drawing.Point(442, 81);
             this.buttonToggle.Name = "buttonToggle";
-            this.buttonToggle.Size = new System.Drawing.Size(200, 55);
+            this.buttonToggle.Size = new System.Drawing.Size(75, 23);
             this.buttonToggle.TabIndex = 9;
             this.buttonToggle.Tag = "Click to toggle Answer";
             this.buttonToggle.Text = "Hide";
@@ -128,10 +122,9 @@
             this.buttonExit.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.buttonExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonExit.ForeColor = System.Drawing.Color.White;
-            this.buttonExit.Location = new System.Drawing.Point(1179, 911);
-            this.buttonExit.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.buttonExit.Location = new System.Drawing.Point(442, 382);
             this.buttonExit.Name = "buttonExit";
-            this.buttonExit.Size = new System.Drawing.Size(200, 55);
+            this.buttonExit.Size = new System.Drawing.Size(75, 23);
             this.buttonExit.TabIndex = 10;
             this.buttonExit.Tag = "Exit Application";
             this.buttonExit.Text = "Exit";
@@ -145,21 +138,22 @@
             this.comboBoxAcronym.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBoxAcronym.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.comboBoxAcronym.FormattingEnabled = true;
-            this.comboBoxAcronym.Location = new System.Drawing.Point(756, 128);
-            this.comboBoxAcronym.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.comboBoxAcronym.Location = new System.Drawing.Point(284, 54);
             this.comboBoxAcronym.Name = "comboBoxAcronym";
-            this.comboBoxAcronym.Size = new System.Drawing.Size(623, 39);
+            this.comboBoxAcronym.Size = new System.Drawing.Size(236, 21);
             this.comboBoxAcronym.TabIndex = 12;
+            this.comboBoxAcronym.SelectedIndexChanged += new System.EventHandler(this.comboBoxAcronym_SelectedIndexChanged);
             // 
             // comboBoxSiteNavigation
             // 
             this.comboBoxSiteNavigation.DisplayMember = "Pages_Name";
             this.comboBoxSiteNavigation.FormattingEnabled = true;
-            this.comboBoxSiteNavigation.Location = new System.Drawing.Point(938, 53);
+            this.comboBoxSiteNavigation.Location = new System.Drawing.Point(352, 22);
+            this.comboBoxSiteNavigation.Margin = new System.Windows.Forms.Padding(1);
             this.comboBoxSiteNavigation.Name = "comboBoxSiteNavigation";
-            this.comboBoxSiteNavigation.Size = new System.Drawing.Size(441, 39);
+            this.comboBoxSiteNavigation.Size = new System.Drawing.Size(168, 21);
             this.comboBoxSiteNavigation.TabIndex = 13;
-            this.comboBoxSiteNavigation.SelectedIndexChanged += new System.EventHandler(this.comboBoxSiteNavigation_SelectedIndexChanged);
+            this.comboBoxSiteNavigation.SelectedIndexChanged += new System.EventHandler(this.comboBoxSiteNavigation_SelectedIndexChanged_1);
             // 
             // buttonSave
             // 
@@ -167,10 +161,9 @@
             this.buttonSave.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.buttonSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonSave.ForeColor = System.Drawing.Color.White;
-            this.buttonSave.Location = new System.Drawing.Point(477, 911);
-            this.buttonSave.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.buttonSave.Location = new System.Drawing.Point(179, 382);
             this.buttonSave.Name = "buttonSave";
-            this.buttonSave.Size = new System.Drawing.Size(200, 55);
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
             this.buttonSave.TabIndex = 15;
             this.buttonSave.Tag = "";
             this.buttonSave.Text = "Save";
@@ -183,10 +176,9 @@
             this.buttonNew.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.buttonNew.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonNew.ForeColor = System.Drawing.Color.White;
-            this.buttonNew.Location = new System.Drawing.Point(261, 911);
-            this.buttonNew.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.buttonNew.Location = new System.Drawing.Point(98, 382);
             this.buttonNew.Name = "buttonNew";
-            this.buttonNew.Size = new System.Drawing.Size(200, 55);
+            this.buttonNew.Size = new System.Drawing.Size(75, 23);
             this.buttonNew.TabIndex = 16;
             this.buttonNew.Tag = "";
             this.buttonNew.Text = "New";
@@ -199,10 +191,9 @@
             this.buttonEdit.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.buttonEdit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonEdit.ForeColor = System.Drawing.Color.White;
-            this.buttonEdit.Location = new System.Drawing.Point(45, 911);
-            this.buttonEdit.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.buttonEdit.Location = new System.Drawing.Point(17, 382);
             this.buttonEdit.Name = "buttonEdit";
-            this.buttonEdit.Size = new System.Drawing.Size(200, 55);
+            this.buttonEdit.Size = new System.Drawing.Size(75, 23);
             this.buttonEdit.TabIndex = 17;
             this.buttonEdit.Tag = "";
             this.buttonEdit.Text = "Edit";
@@ -215,10 +206,9 @@
             this.buttonDelete.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.buttonDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonDelete.ForeColor = System.Drawing.Color.White;
-            this.buttonDelete.Location = new System.Drawing.Point(693, 911);
-            this.buttonDelete.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.buttonDelete.Location = new System.Drawing.Point(260, 382);
             this.buttonDelete.Name = "buttonDelete";
-            this.buttonDelete.Size = new System.Drawing.Size(200, 55);
+            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
             this.buttonDelete.TabIndex = 18;
             this.buttonDelete.Tag = "";
             this.buttonDelete.Text = "Delete";
@@ -230,10 +220,9 @@
             this.buttonCancel.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.buttonCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonCancel.ForeColor = System.Drawing.Color.White;
-            this.buttonCancel.Location = new System.Drawing.Point(909, 911);
-            this.buttonCancel.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.buttonCancel.Location = new System.Drawing.Point(341, 382);
             this.buttonCancel.Name = "buttonCancel";
-            this.buttonCancel.Size = new System.Drawing.Size(200, 55);
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
             this.buttonCancel.TabIndex = 19;
             this.buttonCancel.Tag = "";
             this.buttonCancel.Text = "Cancel";
@@ -242,22 +231,23 @@
             // 
             // textBoxAcronym
             // 
-            this.textBoxAcronym.Location = new System.Drawing.Point(540, 203);
+            this.textBoxAcronym.Location = new System.Drawing.Point(202, 85);
+            this.textBoxAcronym.Margin = new System.Windows.Forms.Padding(1);
             this.textBoxAcronym.Name = "textBoxAcronym";
             this.textBoxAcronym.ReadOnly = true;
-            this.textBoxAcronym.Size = new System.Drawing.Size(623, 38);
+            this.textBoxAcronym.Size = new System.Drawing.Size(236, 20);
             this.textBoxAcronym.TabIndex = 20;
             this.textBoxAcronym.Text = "Acronym";
             this.textBoxAcronym.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // Acronyms
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.Color.SeaGreen;
-            this.ClientSize = new System.Drawing.Size(1423, 999);
+            this.ClientSize = new System.Drawing.Size(534, 419);
             this.Controls.Add(this.textBoxAcronym);
             this.Controls.Add(this.buttonCancel);
             this.Controls.Add(this.buttonDelete);
@@ -274,7 +264,6 @@
             this.Controls.Add(this.label1);
             this.Cursor = System.Windows.Forms.Cursors.Hand;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.Name = "Acronyms";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "CEMS Study App";
--- CEMSStudyApp/Pages/Acronyms.cs
+++ CEMSStudyApp/Pages/Acronyms.cs
@@ -11,18 +11,42 @@ namespace CEMSStudyApp.Pages
         {
             InitializeComponent();
 
-            string connectionString = null;
+            //LOAD COMBOBOX PAGES
+            var pagesDataSet = LoadTable("Pages");
+            comboBoxSiteNavigation.DataSource = pagesDataSet.Tables["Pages"];
+            comboBoxSiteNavigation.ValueMember = "Pages_Id";
+            comboBoxSiteNavigation.DisplayMember = "Pages_Name";
+
+            //LOAD COMBOBOX ACRONYMS
+            var aDataSet = LoadTable("Acronyms");
+            comboBoxAcronym.DataSource = aDataSet.Tables[0];
+            comboBoxAcronym.ValueMember = "Acronyms_Id";
+            comboBoxAcronym.DisplayMember = "Acronyms_Name";
+
+            //LOAD TEXTBOXES
+            textBoxAcronym.Text = aDataSet.Tables[0].Rows[0]["Acronyms_Name"].ToString();
+            textBoxAnswer.Text = aDataSet.Tables[0].Rows[0]["Acronyms_Description"].ToString();
+
+            var index = comboBoxAcronym.SelectedIndex;
+
+            var newIndex = (int)aDataSet.Tables[0].Rows[index]["Acronyms_Id"] - 1;
+
+            MessageBox.Show("Combobox Index: " + index);
+            MessageBox.Show("Row Index: " + newIndex);
+        }
+
+        //CONNECTS TO DB AND LOADS DATA SET
+        public DataSet LoadTable(string nameOfTable)
+        {
+            var tableName = nameOfTable;
             SqlConnection connection;
             SqlCommand command;
             SqlDataAdapter adapter = new SqlDataAdapter();
             DataSet ds = new DataSet();
-            DataTable dtPages = new DataTable("Pages");
-            DataTable dtAcronyms = new DataTable("Acronyms");
-            int i;
-            string sql = null;
+            string sql;
 
             //SET CONNECTION STRING IN PROJECT > APP PROPERTIES > SETTINGS
-            connectionString = Properties.Settings.Default.LocalDb;
+            var connectionString = Properties.Settings.Default.LocalDb;
 
             connection = new SqlConnection(connectionString);
 
@@ -30,41 +54,23 @@ namespace CEMSStudyApp.Pages
             {
                 connection.Open();
 
-                sql = "Select * from Pages";
+                sql = "Select * from " + tableName;
                 command = new SqlCommand(sql, connection);
                 adapter.SelectCommand = command;
-                adapter.Fill(ds, dtPages.ToString());
-
-                sql = "Select * from Acronyms";
-                adapter.SelectCommand.CommandText = sql;
-                adapter.Fill(ds, dtAcronyms.ToString());
+                adapter.Fill(ds, tableName);
 
                 adapter.Dispose();
                 command.Dispose();
                 connection.Close();
-
-                for (i = 0; i <= ds.Tables[0].Rows.Count - 1; i++)
-                {
-                    MessageBox.Show(ds.Tables[0].Rows[i].ItemArray[0] + " -- " + ds.Tables[0].Rows[i].ItemArray[1]);
-
-                }
-
-                for (i = 0; i <= ds.Tables[1].Rows.Count - 1; i++)
-                {
-                    MessageBox.Show(ds.Tables[1].Rows[i].ItemArray[0] + " -- " + ds.Tables[1].Rows[i].ItemArray[1]);
-
-                }
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Can not open connection ! ");
+                MessageBox.Show("Can not open connection !! ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
             }
 
-            comboBoxSiteNavigation.DataSource = ds.Tables[0];
-            comboBoxSiteNavigation.ValueMember = "Pages_Id";
-            comboBoxSiteNavigation.DisplayMember = "Pages_Name";
-
+            return ds;
         }
+
 
         private void EnableTextBoxes()
         {
@@ -104,48 +110,12 @@ namespace CEMSStudyApp.Pages
 
         private void buttonExit_Click(object sender, EventArgs e)
         {
-            DialogResult dr = MessageBox.Show("Are You Sure?", "Exit Application", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            DialogResult dr = MessageBox.Show("Are You Sure?", "Exit Application", MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
 
             if (dr == DialogResult.Yes)
             {
                 Application.Exit();
-            }
-        }
-
-        private void comboBoxSiteNavigation_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            switch (comboBoxSiteNavigation.Text)
-            {
-                //case "Acronyms":
-                //    this.Hide();
-                //    Acronyms acronyms = new Acronyms();
-                //    acronyms.Show();
-                //    break;
-                case "Formulas":
-                    this.Hide();
-                    Formulas formulas = new Formulas();
-                    formulas.Show();
-                    break;
-                case "How To's":
-                    this.Hide();
-                    HowTos howTos = new HowTos();
-                    howTos.Show();
-                    break;
-                case "Main Menu":
-                    this.Hide();
-                    MainMenu mainMenu = new MainMenu();
-                    mainMenu.Show();
-                    break;
-                case "Part 60":
-                    this.Hide();
-                    Part60 part60 = new Part60();
-                    part60.Show();
-                    break;
-                case "Part 75":
-                    this.Hide();
-                    Part75 part75 = new Part75();
-                    part75.Show();
-                    break;
             }
         }
 
@@ -202,13 +172,14 @@ namespace CEMSStudyApp.Pages
         {
             if (buttonEdit.Visible)
             {
-               
+
                 try
                 {
                     Validate();
-                  
-                    MessageBox.Show("Update successful","CEMS Study App", MessageBoxButtons.OK,MessageBoxIcon.Information);
-                  
+
+                    MessageBox.Show("Update successful", "CEMS Study App", MessageBoxButtons.OK,
+                        MessageBoxIcon.Information);
+
                 }
                 catch (Exception)
                 {
@@ -218,7 +189,7 @@ namespace CEMSStudyApp.Pages
 
             if (buttonNew.Visible)
             {
-                
+
             }
 
             DisableTextBoxes();
@@ -228,6 +199,59 @@ namespace CEMSStudyApp.Pages
         private void buttonBack_Click(object sender, EventArgs e)
         {
             throw new NotImplementedException();
+        }
+
+        private void comboBoxAcronym_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var aDataSet = LoadTable("Acronyms");
+            var index = comboBoxAcronym.SelectedIndex;
+
+            textBoxAcronym.Text = aDataSet.Tables[0].Rows[index]["Acronyms_Name"].ToString();
+            textBoxAnswer.Text = aDataSet.Tables[0].Rows[index]["Acronyms_Description"].ToString();
+        }
+
+        //NAVIGATE TO DIFFERENT FORM
+        private void comboBoxSiteNavigation_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            var formName = comboBoxSiteNavigation.SelectedText;
+
+            Hide();
+
+            switch (formName)
+            {
+                //case "Acronyms":
+                //    this.Hide();
+                //    Acronyms acronyms = new Acronyms();
+                //    acronyms.Show();
+                //    break;
+                case "Formulas":
+                    this.Hide();
+                    Formulas formulas = new Formulas();
+                    formulas.Show();
+                    break;
+                case "How To's":
+                    this.Hide();
+                    HowTos howTos = new HowTos();
+                    howTos.Show();
+                    break;
+                case "Main Menu":
+                    this.Hide();
+                    MainMenu mainMenu = new MainMenu();
+                    mainMenu.Show();
+                    break;
+                case "Part 60":
+                    this.Hide();
+                    Part60 part60 = new Part60();
+                    part60.Show();
+                    break;
+                case "Part 75":
+                    this.Hide();
+                    Part75 part75 = new Part75();
+                    part75.Show();
+                    break;
+            }
+
+
         }
     }
 }

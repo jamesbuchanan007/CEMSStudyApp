dc9d2ab2921cf19ebd0a87cd161eeb1f8145d987
James Buchanan <jamesb@spectrumsystems.com>
2019-04-23 15:04:33 -0500

    worked hyperlink page

--- CEMSStudyApp/Pages/DbPassword.cs
+++ CEMSStudyApp/Pages/DbPassword.cs
@@ -11,7 +11,7 @@ namespace CEMSStudyApp.Pages
 
         private void textBoxPassword_KeyPress(object sender, KeyPressEventArgs e)
         {
-            if (e.KeyChar == (char)Keys.Enter)
+            if (e.KeyChar == (char) Keys.Enter)
             {
                 var password = "111";
 
@@ -23,40 +23,38 @@ namespace CEMSStudyApp.Pages
                     var pl = new PasswordsLogin(false);
                     pl.Show();
                 }
-                else
+
+                //USER WANTS TO EDIT DB AFTER ENTERING PASSWORD
+                var openDb = MessageBox.Show("Edit Database?", "CEMS Study App", MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question);
+
+                if (openDb == DialogResult.Yes)
                 {
-                    //USER WANTS TO EDIT DB AFTER ENTERING PASSWORD
-                    var openDb = MessageBox.Show("Edit Database?", "CEMS Study App", MessageBoxButtons.YesNo,
-                        MessageBoxIcon.Question);
-
-                    if (openDb == DialogResult.Yes)
-                    {
-                        Hide();
-                        var export = new ExportDb();
-                        export.Show();
-                    }
-
-                    //USER WANTS TO EDIT DB AFTER ENTERING PASSWORD
-                    var openHyperlink = MessageBox.Show("Remove PDF Hyperlinks?", "CEMS Study App", MessageBoxButtons.YesNo,
-                        MessageBoxIcon.Question);
-
-                    if (openHyperlink == DialogResult.Yes)
-                    {
-                        Hide();
-                        var rpl = new RemovePdfLinks();
-                        rpl.Show();
-                    }
-
-                    //DEFAULTS BACK TO PASSWORD SCREEN
                     Hide();
-                    var pl = new PasswordsLogin(false);
-                    pl.Show();
-
+                    var export = new ExportDb();
+                    export.Show();
+                    return;
                 }
 
-              
-            }
 
+                //USER WANTS TO EDIT DB AFTER ENTERING PASSWORD
+                var openHyperlink = MessageBox.Show("Remove PDF Hyperlinks?", "CEMS Study App", MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question);
+
+                if (openHyperlink == DialogResult.Yes)
+                {
+                    Hide();
+                    var rpl = new RemovePdfLinks();
+                    rpl.Show();
+                    return;
+                }
+
+                //DEFAULTS BACK TO PASSWORD SCREEN
+                Hide();
+                var passLog = new PasswordsLogin(false);
+                passLog.Show();
+
+            }
         }
     }
 }
